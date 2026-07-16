78103e3cc2aaf97f0954edb08e4191b16e242c30
Tran Phung Hoang Vi <tranphunghoangvi2022@gmail.com>
2026-07-16 09:42:32 +0700

    Hoan thanh lab2 bai 1

---
+++ LAB1/testbai.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    int a,b;
+    float x;
+    printf ("nhap vao a b: ");
+    scanf ("%d %d",&a,&b);
+
+    printf ("pt %dx +%d =0 co nghiem x= %.2f",a,b,x=-1.0*b/a);
+}
